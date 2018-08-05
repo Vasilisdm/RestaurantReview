8eb7b874b2ffcf6b637e01035cdca0aebe1de9ee
= <=>
2018-08-05 23:50:05 +0300

    created the file for the service worker

---
+++ swRegistration.js
@@ -0,0 +1,14 @@
+// When the initial HTML document has been completely loaded and parsed.
+document.addEventListener('DOMContentLoaded', function(event){
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/sw.js').then(function(registration){
+            // the registration proccess worked
+            console.log(`Service worker's registration succeeded.`);
+        }).catch(function(error){
+            //  the registration proccess failed
+            console.log(`Service worker's registration failed with error: ${error}.`);
+        });
+    }
+
+})
